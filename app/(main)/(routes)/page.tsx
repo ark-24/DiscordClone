324bbffa7eb0a798e45758b0e5650d6f77e098f8
ark24 <abdul.khan@designitcorp.ca>
2024-01-17 22:05:21 -0800

    Set Up

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div >
+    
+    <p className='text-3xl font-bold text-indigo-500'>Discord Clone</p>
+    <Button variant="outline">Button</Button>
+    </div>
+
+  )
+}
+//26:02
