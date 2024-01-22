e72c6eb9a44bbff9fb7c90a1c2e782541e1c3cc2
ark24 <abdul.khan@designitcorp.ca>
2024-01-21 21:20:15 -0800

    Completed Authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,17 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div >
     
     <p className='text-3xl font-bold text-indigo-500'>Discord Clone</p>
+    <div style={{display:'flex', alignItems:'center'}}>
     <Button variant="outline">Button</Button>
+    <UserButton afterSignOutUrl="/"/>
+    <ModeToggle/>
+    </div>
     </div>
 
   )
