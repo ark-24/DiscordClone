a5ecb388a4a4e45445651c5538e68987d727c94c
ark24 <abdul.khan@designitcorp.ca>
2024-02-07 23:58:59 -0800

    UploadThing Set Up

--- components/modal/inital-modal.tsx
+++ components/modal/inital-modal.tsx
@@ -56,7 +56,18 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                File Upload
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                
+                                />
                             </div>
                             <FormField 
                             control = {form.control}
